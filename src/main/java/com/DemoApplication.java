63f34801d2fd7a9641706f94706d66e0c2bdfc29
董凤文Z1003 <fengwen.dong@going-link.com>
2023-09-09 15:28:35 +0800

    [初始化]

---
+++ src/main/java/com/DemoApplication.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@MapperScan({"com.mig.data.mapper"})
+@EnableScheduling
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/demo/Vavr.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @description:
+ * @author: fengwen.dong@going-link.com
+ * @createDate: 2022-12-24 21:20
+ */
+public class Vavr {
+
+    public static void main(String[] args) {
+
+        String test = "";
+        for (int i = 0; i <300 ; i++) {
+            test  = test+"f"+i+",";
+        }
+        System.out.println(test);
+    }
+
+
+}
